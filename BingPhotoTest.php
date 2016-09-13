e10f7607bd63554f33ad701d0abd161187c3ec0f
Jonathan Gruber <jonathan.gruber@ercas.de>
2016-09-13 16:53:16 +0200

    Added tests for argument validation and general cleanup

--- BingPhotoTest.php
+++ BingPhotoTest.php
@@ -1,64 +1,141 @@
 <?php
 require('BingPhoto.php');
 
-class BingPhotoTest extends PHPUnit_Framework_TestCase {
+class BingPhotoTest extends PHPUnit_Framework_TestCase
+{
 
-    protected $bp;
+    /**
+     * @var BingPhoto
+     */
+    protected $bing;
 
-    protected function setUp() {
-        $this->bp = new BingPhoto();
+    protected function setUp()
+    {
+        $this->bing = new BingPhoto();
+    }
+
+    /**
+     * @dataProvider invalidArgumentProvider
+     * @param $expected
+     * @param $args
+     */
+    public function testArgsValidation($expected, $args = [])
+    {
+        $this->bing->setArgs($args);
+        $actual = $this->bing->getArgs();
+
+        foreach ($expected as $key => $expectedArg) {
+            $this->assertEquals($expectedArg, $actual[$key]);
+        }
     }
 
     /**
      * @dataProvider argumentProvider
      * @param $args
      */
-    public function testCount($args = array()) {
-        $this->bp->setArgs($args);
+    public function testCount($args = [])
+    {
+        $this->bing->setArgs($args);
         $count = isset($args['n']) ? $args['n'] : 1;
-        $this->assertCount($count, $this->bp->getImages());
+        $this->assertCount($count, $this->bing->getImages());
     }
 
     /**
      * @dataProvider argumentProvider
      * @param $args
      */
-    public function testResolution($args = array()) {
-        $this->bp->setArgs($args);
-        foreach ($this->bp->getImages() as $image) {
+    public function testResolution($args = [])
+    {
+        $this->bing->setArgs($args);
+        foreach ($this->bing->getImages() as $image) {
             if (isset($args['resolution'])) {
-                list($resX, $resY) = getimagesize($image['url']);
-                $this->assertEquals($resX . 'x' . $resY, $args['resolution']);
+                list($width, $height) = getimagesize($image['url']);
+                $this->assertEquals($width . 'x' . $height, $args['resolution']);
             }
         }
     }
 
-    public function argumentProvider() {
-        return array(
-            'no arguments' => array(),
-            'low resolution, de-DE' => array(
-                array (
+    public function invalidArgumentProvider()
+    {
+        return [
+            'invalid date in future' => [
+                [
+                    'date' => -1
+                ],
+                [
+                    'date' => -2
+                ]
+            ],
+            'n too large' => [
+                [
+                    'n' => 8
+                ],
+                [
+                    'n' => 9
+                ]
+            ],
+            'n zero' => [
+                [
+                    'n' => 1
+                ],
+                [
+                    'n' => 0
+                ]
+            ],
+            'n negative' => [
+                [
+                    'n' => 1
+                ],
+                [
+                    'n' => -2
+                ]
+            ],
+            'unavailable resolution' => [
+                [
+                    'resolution' => '1920x1080'
+                ],
+                [
+                    'resolution' => '1920x1200'
+                ]
+            ],
+            'invalid resolution' => [
+                [
+                    'resolution' => '1920x1080'
+                ],
+                [
+                    'resolution' => 'foo'
+                ]
+            ],
+        ];
+    }
+
+    public function argumentProvider()
+    {
+        return [
+            'no arguments' => [],
+            'low resolution, de-DE' => [
+                [
                     'resolution' => BingPhoto::RESOLUTION_LOW,
                     'locale' => 'de-DE'
-                )
-            ),
-            'high resolution, fr-FR' => array(
-                array (
+                ]
+            ],
+            'high resolution, fr-FR' => [
+                [
                     'resolution' => BingPhoto::RESOLUTION_HIGH,
                     'locale' => 'fr-FR'
-                )
-            ),
-            'three last images' => array(
-                array (
+                ]
+            ],
+            'three last images' => [
+                [
                     'n' => 3
-                )
-            ),
-            'yesterday\'s image' => array(
-                array (
+                ]
+            ],
+            'yesterday\'s image' => [
+                [
                     'n' => 1,
                     'date' => BingPhoto::YESTERDAY
-                )
-            ),
-        );
+                ]
+            ],
+        ];
     }
 }
